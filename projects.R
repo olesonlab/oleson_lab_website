98066b533569e408099204f62ad6eb6a5ce80caf
Oleson Lab Admin - AC <25646578+olesonlab@users.noreply.github.com>
2025-08-04 12:47:17 +0000

    chore: update yaml

---
+++ projects.R
@@ -0,0 +1,18 @@
+---
+title: "{{< bi folder-open-fill >}} Projects"
+description: "As data are compiled for our projects, they will be posted here."
+page-layout: full
+listing:
+  contents: projects
+  feed: true
+  type: grid
+  sort: "date desc"
+  grid-item-align: left
+  image-placeholder: assets/images/placeholder-image.png
+  categories: numbered
+  date-format: iso
+  grid-columns: 1
+  image-height: 450px
+---
+  
+  
